824d6ec884fbbe707fcc4f95648a7cb596cb61c8
AYOU-cell <a2487806917@gmail.com>
2025-12-24 13:52:23 +0800

    Update App.tsx

--- ez-fb (1)/src/App.tsx
+++ ez-fb (1)/src/App.tsx
@@ -6,10 +6,9 @@ import { Footer } from './components/Footer';
 import { LanguageProvider } from './contexts/LanguageContext';
 
 export default function App() {
+  // Meta Pixel (Facebook)
   useEffect(() => {
-    // Meta Pixel Code - ensure it only initializes once
     if ((window as any).fbq) {
-      // Pixel already loaded, skip initialization
       return;
     }
 
@@ -33,6 +32,54 @@ export default function App() {
     
     (window as any).fbq('init', '650527014720673');
     (window as any).fbq('track', 'PageView');
+  }, []);
+
+  // TikTok Pixel
+  useEffect(() => {
+    // 防止重复初始化
+    if ((window as any).ttq) return;
+
+    const pixelId = 'D53CCDBC77U0DP19B9L0'; // 你的 TikTok Pixel ID
+
+    (function (w: any, d: any, t: any) {
+      w.TiktokAnalyticsObject = t;
+      var ttq = w[t] = w[t] || [];
+      ttq.methods = [
+        "page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie", "holdConsent", "revokeConsent", "grantConsent"
+      ];
+      ttq.setAndDefer = function (t: any, e: any) {
+        t[e] = function () {
+          t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+        };
+      };
+      for (var i = 0; i < ttq.methods.length; i++)
+        ttq.setAndDefer(ttq, ttq.methods[i]);
+      ttq.instance = function (t: any) {
+        for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++)
+          ttq.setAndDefer(e, ttq.methods[n]);
+        return e;
+      };
+      ttq.load = function (e: any, n: any) {
+        var r = "https://analytics.tiktok.com/i18n/pixel/events.js";
+        ttq._i = ttq._i || {};
+        ttq._i[e] = [];
+        ttq._i[e]._u = r;
+        ttq._t = ttq._t || {};
+        ttq._t[e] = +new Date();
+        ttq._o = ttq._o || {};
+        ttq._o[e] = n || {};
+        
+        var c = d.createElement("script");
+        c.type = "text/javascript";
+        c.async = true;
+        c.src = r + "?sdkid=" + e + "&lib=" + t;
+        var s = d.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(c, s);
+      };
+
+      ttq.load(pixelId);
+      ttq.page();
+    })(window, document, 'ttq');
   }, []);
 
   return (
